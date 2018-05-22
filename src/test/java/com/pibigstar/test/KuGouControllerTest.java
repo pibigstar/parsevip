65f28a1ae52ccf17615afc0bf5e9bb7b860c30a4
pibigstar <leikewei123@163.com>
2018-05-22 12:25:39 +0800

    单元测试

--- src/test/java/com/pibigstar/test/KuGouControllerTest.java
+++ src/test/java/com/pibigstar/test/KuGouControllerTest.java
@@ -1,28 +1,26 @@
 package com.pibigstar.test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.pibigstar.web.MusicController;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class KuGouControllerTest {
-	
+	@Autowired
 	private MockMvc mvc;
-	@Before
-	public void setUp() {
-		mvc = MockMvcBuilders.standaloneSetup(new MusicController()).build();
-	}
 	
 	@Test
 	public void testKugouController() throws Exception{
@@ -31,9 +29,7 @@ public class KuGouControllerTest {
 		request = post("/music/seach")
 				.param("type", "0")
 				.param("music", "说散就散");
-		mvc.perform(request).andExpect(status().isOk());
-		
+		mvc.perform(request).andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON));
 	}
-	
-
 }
