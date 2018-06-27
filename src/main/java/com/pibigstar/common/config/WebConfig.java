5b5747d79f9d30b29c3f0d276a3b3db9b181e964
pibigstar <leikewei123@163.com>
2018-06-27 15:47:15 +0800

    新增xss过滤

--- src/main/java/com/pibigstar/common/config/WebConfig.java
+++ src/main/java/com/pibigstar/common/config/WebConfig.java
@@ -1,10 +1,15 @@
 package com.pibigstar.common.config;
 
+import java.util.Map;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.google.common.collect.Maps;
 import com.pibigstar.common.Constant;
 
 @Configuration
@@ -18,6 +23,25 @@ public class WebConfig implements WebMvcConfigurer{
 		//配置server虚拟路径，handler为jsp中访问的目录，locations为files相对应的本地路径     
 		registry.addResourceHandler("/files/**").addResourceLocations("file:///"+Constant.DEFAULT_FILE_UPLOAD_PATH);  
 	}
+	
+	/**
+	 * xss过滤拦截器
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Bean
+	public FilterRegistrationBean xssFilterRegistrationBean() {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new XssFilter());
+		filterRegistrationBean.setOrder(1);
+		filterRegistrationBean.setEnabled(true);
+		filterRegistrationBean.addUrlPatterns("/*");
+		Map<String, String> initParameters = Maps.newHashMap();
+		initParameters.put("excludes", "/favicon.ico,/img/*,/js/*,/css/*");
+		initParameters.put("isIncludeRichText", "true");
+		filterRegistrationBean.setInitParameters(initParameters);
+		return filterRegistrationBean;
+	}
+	
 	
 	/**
 	 * 拦截器配置
